ed7bc9cecdf08df43da808367479d9a110f08d85
Chriford Siame <143299729+chriford-siame@users.noreply.github.com>
2024-08-21 23:28:22 +0200

    Update search form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,11 +12,14 @@
                     <div class="row align-items-center">
                         <div class="col-lg-7 portfolio-info">
                             <h3>Browse Movie Trailers</h3>
-                            <p>Voluptatem sit minima et dignissimos earum fuga vel nulla quidem aut quam</p>
+                            <p>Browse trailers from reliable & trusted sources</p>
                         </div>
                         <div class="col-lg-5 text-center text-lg-end">
                             <div>
-                                <input type="text" class="form-control min-vw-50" data-aos="fade-down" placeholder="Search trailer">
+                                <div class="input-group mb-3" data-aos="fade-down">
+                                    <input placeholder="Search trailer" type="text" class="form-control" aria-label="">
+                                    <button type="button" class="btn input-group-text btn-info text-white"> <i class="bi bi-search"></i></button>
+                                  </div>
                             </div>
                             <div class="row">
                                 <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100"> 
