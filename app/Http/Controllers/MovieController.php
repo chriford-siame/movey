9e7f8922419b6a928bee70fc9e274852b052eb54
Chriford Siame <143299729+chriford-siame@users.noreply.github.com>
2024-08-26 01:57:12 +0200

    Add controller methods

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,9 +6,21 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
 
     public function index() {
-        return view('index');
+        return view('home');
     }
+
+    public function movie_list() {
+        return view('movie_list');
+    }
+
+    public function movie_create() {
+        return view('movie_create');
+    }
+
+    public function movie_detail() {
+        return view('movie_detail');
+    }
+    
 }
