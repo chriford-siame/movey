d990d1090a99b2e082bb8f16e280ed95e48d9bc0
Chriford Siame <143299729+chriford-siame@users.noreply.github.com>
2024-08-26 01:10:13 +0200

    update blade

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -52,7 +52,7 @@
     <script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
 
     <!-- Main JS File -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{ url('assets/js/main.js') }}"></script>
 </body>
 
 </html>
